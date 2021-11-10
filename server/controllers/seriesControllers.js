3d4f3a33dd99d2335d325f2432f6d82e0924ea4e
AipBorges <mpbds@uic.es>
2021-11-10 19:44:56 +0100

    created delete serie controller

--- server/controllers/seriesControllers.js
+++ server/controllers/seriesControllers.js
@@ -5,4 +5,22 @@ const getSeries = async (req, res) => {
   res.json(series);
 };
 
-module.exports = { getSeries };
+const deleteSerie = async (req, res, next) => {
+  const { idSerie } = req.params;
+  try {
+    const deletedSerie = await Serie.findByIdAndDelete(idSerie);
+    if (deletedSerie) {
+      res.json({ id: deleteSerie.id });
+    } else {
+      const error = new Error("Wrong series! SO typical of you...");
+      error.code = 404;
+      next(error);
+    }
+  } catch (error) {
+    error.code = 400;
+    error.message = "Ooooooooooooh! Caught in a Bad Request!";
+    next(error);
+  }
+};
+
+module.exports = { getSeries, deleteSerie };
