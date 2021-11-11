734bc0aa7cc96d1002edbeb38e8370bcc9b028f9
AipBorges <mpbds@uic.es>
2021-11-11 10:58:10 +0100

    auth done. needs testing

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const { validate } = require("express-validation");
+/* const logInController = require("../controller/logInController");
+const loginSchema = require("../Schema/userSchemas"); */
+
+const router = express.Router();
+
+/* const User = require("../../database/models/user");
+ router.get("/", async () => {
+  User.create({
+    name: "MiBot",
+    username: "botMolon23",
+    password: await bcrypt.hash("soyRobot45!", 10),
+  });
+}); */
+
+/* router.post("/login", validate(loginSchema, {}, {}), logInController); */
+
+module.exports = router;
